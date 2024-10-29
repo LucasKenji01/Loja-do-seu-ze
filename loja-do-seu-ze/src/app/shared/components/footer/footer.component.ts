fac2dc9eab864d51dfe5958938f1d2f18fd78562
Lucas Kenji <lucasknishida@gmail.com>
2024-10-29 11:44:38 -0300

    Header e Footer estilizados

--- loja-do-seu-ze/src/app/shared/components/footer/footer.component.ts
+++ loja-do-seu-ze/src/app/shared/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  constructor(private router: Router) { }
 
+  ngOnInit(): void {
+  }
+
+  navigateByUrl(url: string) {
+    this.router.navigateByUrl(url)
+  }
 }
